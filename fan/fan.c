73bd298c5d1544eea1fcecf9279cb4a104f1f2c9
Greg Prisament <greg@canopy.link>
2014-12-21 22:55:25 -0800

    Hack to get fan_speed variable working, and add newlines to printfs

--- fan/fan.c
+++ fan/fan.c
@@ -3,7 +3,7 @@
 
 static void SetFanSpeed(int8_t speed)
 {
-    printf("Fan speed set to %d", speed);
+    printf("Fan speed set to %d\n", speed);
 }
 
 static bool ReadSensor(float *temperature, float *humidity)
@@ -20,7 +20,7 @@ static int OnFanSpeedChange(CanopyContext ctx, const char *varName, void *userDa
 
     result = canopy_var_get_int8(ctx, "fan_speed", &fanSpeed);
     if (!ctx) {
-        fprintf(stderr, "Error reading fan_speed");
+        fprintf(stderr, "Error reading fan_speed\n");
         return -1;
     }
 
@@ -37,7 +37,7 @@ int main(void)
 
     ctx = canopy_init_context();
     if (!ctx) {
-        fprintf(stderr, "Error initializing context");
+        fprintf(stderr, "Error initializing context\n");
         return -1;
     }
 
@@ -48,49 +48,58 @@ int main(void)
     );
 
     if (result != CANOPY_SUCCESS){
-        fprintf(stderr, "Failed to configure context");
+        fprintf(stderr, "Failed to configure context\n");
         return -1;
     }
 
     result = canopy_var_init(ctx, "out float32 temperature");
     if (result != CANOPY_SUCCESS){
-        fprintf(stderr, "Failed to init cloudvar 'temperature'");
+        fprintf(stderr, "Failed to init cloudvar 'temperature'\n");
         return -1;
     }
     result = canopy_var_init(ctx, "out float32 humidity");
     if (result != CANOPY_SUCCESS){
-        fprintf(stderr, "Failed to init cloudvar 'humidity'");
+        fprintf(stderr, "Failed to init cloudvar 'humidity'\n");
         return -1;
     }
-    result = canopy_var_init(ctx, "in int8 fan_speed");
+    result = canopy_var_init(ctx, "inout int8 fan_speed");
     if (result != CANOPY_SUCCESS){
-        fprintf(stderr, "Failed to init cloudvar 'fan_speed'");
+        fprintf(stderr, "Failed to init cloudvar 'fan_speed'\n");
         return -1;
     }
 
     result = canopy_var_on_change(ctx, "fan_speed", OnFanSpeedChange, NULL);
     if (result != CANOPY_SUCCESS) {
-        fprintf(stderr, "Error setting up fan_speed callback");
+        fprintf(stderr, "Error setting up fan_speed callback\n");
+        return -1;
+    }
+
+    // turn fan off
+    result = canopy_var_set_int8(ctx, "fan_speed", 0);
+    if (!ctx) {
+        fprintf(stderr, "Error setting fan_speed\n");
         return -1;
     }
 
     while (1) {
         canopy_sync_blocking(ctx, 10*CANOPY_SECONDS);
 
-        if (canopy_once_every(&reportTimer, 60*CANOPY_SECONDS)) {
+        if (canopy_once_every(&reportTimer, 5*CANOPY_SECONDS)) {
+            printf("reading sensors...\n");
             float temperature, humidity;
             bool sensorOk;
             sensorOk = ReadSensor(&temperature, &humidity);
             if (sensorOk) {
+                printf("Temperature: %f    Humidity: %f\n", temperature, humidity);
                 result =canopy_var_set_float32(ctx, "temperature", temperature);
                 if (result != CANOPY_SUCCESS) {
-                    fprintf(stderr, "Failed to set cloudvar 'temperature'");
+                    fprintf(stderr, "Failed to set cloudvar 'temperature'\n");
                     return -1;
                 }
 
                 result = canopy_var_set_float32(ctx, "humidity", humidity);
                 if (result != CANOPY_SUCCESS) {
-                    fprintf(stderr, "Failed to set cloudvar 'humidity'");
+                    fprintf(stderr, "Failed to set cloudvar 'humidity'\n");
                     return -1;
                 }
             }
